a5d045c1ec37beea9c3fdc8f226579e44b5bda87
DragonGames85 <dragongames85@mail.ru>
2023-02-10 20:10:18 +0700

    added jest

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-import { Suspense } from 'react';
-import './styles/index.scss';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
-import { AppRouter } from './providers/router';
+import { Suspense } from "react";
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
