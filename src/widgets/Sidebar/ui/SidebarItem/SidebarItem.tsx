34c893abf2b8b5e56f02940f01c5b88a497f6a7f
DragonGames85 <dragongames85@mail.ru>
2023-03-23 12:47:18 +0700

    fix sidebar

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -2,10 +2,10 @@ import { useTranslation } from 'react-i18next';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './SidebarItem.module.scss';
-import { SidebarItemType } from '../../model/items';
 import { useSelector } from 'react-redux';
 import { getUserAuthData } from 'entities/User';
+import { SidebarItemType } from 'widgets/Sidebar/model/types/sidebar';
+import cls from './SidebarItem.module.scss';
 
 interface SidebarItemProps {
     item: SidebarItemType;
@@ -14,10 +14,9 @@ interface SidebarItemProps {
 
 export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
     const { t } = useTranslation();
-
     const isAuth = useSelector(getUserAuthData);
 
-    if(item.authOnly && !isAuth) {
+    if (item.authOnly && !isAuth) {
         return null;
     }
 
@@ -25,7 +24,7 @@ export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
         <AppLink
             theme={AppLinkTheme.SECONDARY}
             to={item.path}
-            className={classNames(cls.item, { [cls.collapsed]: collapsed }, [])}
+            className={classNames(cls.item, { [cls.collapsed]: collapsed })}
         >
             <item.Icon className={cls.icon} />
             <span className={cls.link}>
