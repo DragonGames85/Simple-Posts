221ff6000d282be8bc2d1d165a7dcbf2bd62ca88
dragongames85 <dragongames85@mail.ru>
2023-07-12 19:28:37 +0500

    minor fixes

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,17 +1,17 @@
 module.exports = {
-    // 1️⃣ Path to your story files
+    // 1 Path to your story files
     stories: [
         '../../src/**/*.stories.@(js|jsx|ts|tsx)',
     ],
-    // 2️⃣ Path to your component files
+    // 2 Path to your component files
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
     ],
-    // 3️⃣ Path to your webpack configuration
+    // 3 Path to your webpack configuration
     framework: '@storybook/react',
-    // 4️⃣ Path to your webpack configuration
+    // 4 Path to your webpack configuration
     core: {
         builder: 'webpack5',
     },
