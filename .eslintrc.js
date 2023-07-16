396fc903dda9c413bb131a0cfa1814a4af5e071e
dragongames85 <dragongames85@mail.ru>
2023-07-16 17:52:48 +0500

    add headless ui, add listbox component

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true, // только ругаться в файлах jsx
-                ignoreAttribute: ['role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'], // игнорировать переводы в атрибутах 'data-testid', 'to'
+                ignoreAttribute: ['as', 'role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'], // игнорировать переводы в атрибутах 'data-testid', 'to'
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 120 }], // максимальная длина строки
