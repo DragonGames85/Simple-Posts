ef5243eb282f3f1f5696c32fe8aae794831a695c
dragongames85 <dragongames85@mail.ru>
2023-07-31 18:51:24 +0500

    add json settings

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,10 @@ import React, { Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { getUserInited, userActions } from '@/entities/User';
+import { AppRouter } from './providers/router';
 import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
-import { AppRouter } from './providers/router';
-import { useTheme } from './providers/ThemeProvider';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 
 function App() {
     const { theme } = useTheme();
