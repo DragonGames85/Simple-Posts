f5b919a61a62a4373f397fbbde37c1cd0b4c92b5
DragonGames85 <dragongames85@mail.ru>
2023-09-03 23:06:23 +0700

    fix icons size and color

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,7 +9,22 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     // https://www.npmjs.com/package/%40svgr/webpack для svg файлов
     const svgLoader = {
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        use: [{
+            loader: '@svgr/webpack',
+            options: {
+                icon: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: 'convertColors',
+                            params: {
+                                currentColor: true,
+                            }
+                        }
+                    ]
+                }
+            }
+        }],
     };
 
     // babel loader
